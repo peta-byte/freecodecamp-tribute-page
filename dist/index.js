338bef8e753b53b35bd8158f903d2f24fa19142b
Chepkorir Brenda <40006302+ChepkorirBrenda@users.noreply.github.com>
2019-12-30 09:13:32 -0600

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8081;
 
 app.use(express.json());
 app.use('/', express.static(path.join(__dirname, '.')));
